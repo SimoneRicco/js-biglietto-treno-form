cdd8bc129287082895acb11af95af9a2299c3aaf
SimoneRicco <smnrcc0@gmail.com>
2023-04-13 14:59:54 +0200

    add form

---
+++ assets/tools.js
@@ -0,0 +1,23 @@
+const nomeCognome = document.querySelector("#name-surname");
+const nKm = document.querySelector("#name-surname");
+const age = document.querySelector("#age").value;
+const priceAtKm = 0.21;
+let finalPrice;
+document.querySelector("#generate").addEventListener("click",function(){
+    console.log(nomeCognome + " + " + nKm + " + " + age);
+    if (nKm > 0) {
+        // inizio calcolo
+        if (age < 18) {
+            finalPrice = (priceAtKm * nKm) - ((priceAtKm * nKm * 20) / 100);
+        } else {
+            if (age > 65) {
+                finalPrice = (priceAtKm * nKm) - ((priceAtKm * nKm * 40) / 100);
+            } else {
+                finalPrice = priceAtKm * nKm;
+            }
+        }
+        document.getElementById("output").innerHTML = "Il prezzo è di: " + finalPrice + " euro";
+    } else {
+        alert("Numero di kilometri non valido");
+    }
+});
