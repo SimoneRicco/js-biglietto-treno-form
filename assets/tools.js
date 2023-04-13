0151759ca34469749b1eea8eaef935d290d45f41
SimoneRicco <smnrcc0@gmail.com>
2023-04-13 16:20:48 +0200

    fix javascript biglietto

--- assets/tools.js
+++ assets/tools.js
@@ -1,22 +1,23 @@
-const nomeCognome = document.querySelector("#name-surname").value;
-const nKm = document.querySelector("#name-surname").value;
-const age = document.querySelector("#age").value;
-const priceAtKm = 0.21;
-let finalPrice;
-document.querySelector("#generate").addEventListener("click",function(){
+document.querySelector("#generate").addEventListener("click", function () {
+    const nomeCognome = document.querySelector("#name-surname").value;
+    const nKm = document.querySelector("#km").value;
+    const age = document.querySelector("#age").value;
+    const priceAtKm = 0.21;
+    let finalPrice;
+    let discount;
     console.log(nomeCognome + " + " + nKm + " + " + age);
     if (nKm > 0) {
-        // inizio calcolo
-        if (age < 18) {
-            finalPrice = (priceAtKm * nKm) - ((priceAtKm * nKm * 20) / 100);
+        if (age == "min") {
+            discount = 20;
+            finalPrice = (priceAtKm * nKm) - ((priceAtKm * nKm * discount) / 100);
         } else {
-            if (age > 65) {
-                finalPrice = (priceAtKm * nKm) - ((priceAtKm * nKm * 40) / 100);
-            } else {
-                finalPrice = priceAtKm * nKm;
+            if (age == "mag") {
+                discount = 40;
+                finalPrice = (priceAtKm * nKm) - ((priceAtKm * nKm * discount) / 100);
             }
         }
-        document.getElementById("output").innerHTML = "Il prezzo è di: " + finalPrice + " euro";
+        console.log(finalPrice);
+        document.querySelector(".output").innerHTML = "Biglietto di " + nomeCognome + ": Sconto del " + discount + "%, Carrozza N" + (Math.floor(Math.random() * 21) + 1) + ", Codice CP " + (Math.floor(Math.random() * 100000) + 1) + ", Prezzo: " + finalPrice + "€";
     } else {
         alert("Numero di kilometri non valido");
     }
